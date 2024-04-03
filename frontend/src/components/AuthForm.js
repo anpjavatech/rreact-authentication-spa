e6f915bbfe2aaf384f504f74cf578ebd46913e47
Anoop Kuttikattu Sukumaran Nair <anpks@Anoops-Air.box>
2024-04-03 22:25:31 +0200

    Commit after adding the basic authentication form.

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, useSearchParams, NavLink } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
 
   return (
     <>
@@ -23,9 +20,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <NavLink to={`?mode=${isLogin? "signup" : "login"}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </NavLink>
           <button>Save</button>
         </div>
       </Form>
